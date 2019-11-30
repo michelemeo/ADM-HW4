f65eccc7cc6e3c8c6a963baeacdae1e9bf189936
michelemeo <56388365+michelemeo@users.noreply.github.com>
2019-11-30 13:09:11 +0100

    k-means work

---
+++ utils/clustering_lib.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import math
+
+
+def distance(v1, v2):
+    d = 0
+    
+    for i in range(len(v1)):
+        d += (v1[i]-v2[i])**2
+    
+    return math.sqrt(d)
+
+
+class Kmeans:
+
+    def __init__(self, k, n_features):
+        self.C = np.zeros((k, n_features))
+        self.K = k
+        self.n = n_features
+        self.Cluster = {i+1:[] for i in range(k)}
+
+
+    def random_centers(self, data):
+        
+        for f in range(self.n):
+            self.C[:,f] = np.random.uniform(data[:,f].min(), data[:,f].max(), size=(self.K,))
+
+
+    def closest_center(self, vector):
+        distances = np.zeros(self.K)
+        center = 1
+        
+        for k in range(self.K):
+            distances[k] = distance(self.C[k], vector)
+
+            if k > 0 and distances[k] < distances[k-1]:
+                center = k+1
+
+        return center
+
+
+    def clustering(self, data):
+
+        for i in range(len(data)):
+            c = self.closest_center(data[i])
+            self.Cluster[c].append(data[i])
+            
+
+    def update_centers(self):
+
+        for k in range(self.K):
+            midpoint = np.mean(self.Cluster[k+1], axis=0)
+            self.C[k] = midpoint
