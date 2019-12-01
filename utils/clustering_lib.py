166056e8477c611b457d2dd4934c64c856d026e6
michelemeo <56388365+michelemeo@users.noreply.github.com>
2019-12-01 20:27:00 +0100

    Final clustering_lib.py

--- utils/clustering_lib.py
+++ utils/clustering_lib.py
@@ -3,6 +3,7 @@ import pandas as pd
 import math
 
 
+# this function calculates the distance between two vectors
 def distance(v1, v2):
     d = 0
     
@@ -12,8 +13,12 @@ def distance(v1, v2):
     return math.sqrt(d)
 
 
-class Kmeans:
 
+class Kmeans:
+    
+    # once a Kmeans object is created with "k" clusters, an array of zeros is defined
+    # in which we will store the coordinates of the centroids and an empty list in which 
+    # we will insert the points belonging to each of the "k" clusters
     def __init__(self, k, n_features):
         self.C = np.zeros((k, n_features))
         self.K = k
@@ -21,25 +26,31 @@ class Kmeans:
         self.Cluster = {i+1:[] for i in range(k)}
 
 
+    # to initialize centroids randomly, as explained in main.ipynb
     def random_centers(self, data):
         
         for f in range(self.n):
             self.C[:,f] = np.random.uniform(data[:,f].min(), data[:,f].max(), size=(self.K,))
 
 
+    # given a point (or vector) it returns the nearest centroid, or the cluster to which such point belongs
     def closest_center(self, vector):
         distances = np.zeros(self.K)
         center = 1
         
+        # evaluate the distance from each centroid
         for k in range(self.K):
             distances[k] = distance(self.C[k], vector)
 
             if k > 0 and distances[k] < distances[k-1]:
+                # when the evaluated distance becomes the shortest, instead the center is "k+1" 
+                # because the clusters names start from 1 instead of 0 as the index "k"
                 center = k+1
 
         return center
 
 
+    # it scrolls all the data and adds each of them to a cluster in the dictionary created initially
     def clustering(self, data):
 
         for i in range(len(data)):
@@ -47,19 +58,25 @@ class Kmeans:
             self.Cluster[c].append(data[i])
             
 
+    # updates the coordinates of the centroids with the amidpoint of the data belonging 
+    # to the cluster defined by that centroid
     def update_centers(self):
 
         for k in range(self.K):
             midpoint = np.mean(self.Cluster[k+1], axis=0)
             
+            # if a cluster has no element, its centroid is not updated because this gives 
+            # problem in the main code
             if len(self.Cluster[k+1]) != 0:
                 self.C[k] = midpoint
             else:
                 continue
-            
+        
+        # it is necessary to empty the dictionary to fill it with new clusters
         self.Cluster = {i+1:[] for i in range(self.K)}
             
-            
+    
+    # this function evaluates the cost function of k-means algo
     def cost_function(self):
         cost = 0
         
